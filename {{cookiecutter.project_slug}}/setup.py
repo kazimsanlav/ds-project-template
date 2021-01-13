9ecc7f4679079e8cf74a147c6352c239673f94dd
kazimsanlav <kazimsanlav@gmail.com>
2021-01-13 22:11:32 +0300

    setup.py updated

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -4,13 +4,15 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+def read(fname):
+    with open(fname, 'r') as f:
+        return f.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+readme = read("README.rst")
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=7.0',{%- endif %} 'python-dotenv' ]
+history = read("HISTORY.rst")
+
+requirements = read('requirements.txt').splitlines()
 
 setup_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%- endif %} ]
 
@@ -57,7 +59,10 @@ setup(
     include_package_data=True,
     keywords='{{ cookiecutter.project_slug }}',
     name='{{ cookiecutter.project_slug }}',
-    packages=find_packages(where='src'),
+    packages=find_packages(include=['{{ cookiecutter.project_slug }}', '{{ cookiecutter.project_slug }}.*'], exclude=['tests']),
+    # package_data={
+    #     "utils": [],
+    # },
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
